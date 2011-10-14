3a2fb4d9854e14c4deecf55d80ab1bb1155df2c1
LamCiuLoeng <lamciuloeng@gmail.com>
2011-10-14 23:01:19 +0800

    fix the path bug

--- util.php
+++ util.php
@@ -15,8 +15,8 @@
 	
 	function getSmartyInstance(){
 		$smarty =  new Smarty();
-		$smarty->template_dir = dirname(__FILE__).'\templates';
-		$smarty->compile_dir = dirname(__FILE__).'\templates_c';
+		$smarty->template_dir = dirname(__FILE__).'/templates';
+		$smarty->compile_dir = dirname(__FILE__).'/templates_c';
 //		$smarty->debugging = true;
 		return $smarty;		
 	}
